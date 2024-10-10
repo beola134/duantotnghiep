9ddcae7343ceaae4d4d50bdbf8c9311db16bc028
nhut hao <ngonhuthao912004@gmail.com>
2024-10-11 01:58:03 +0700

    thêm api san pham hao 11/10

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -4,6 +4,346 @@ const Cate = require("../models/cate");
 const Category = require("../models/cate");
 const { Op } = require("sequelize");
 
+// show sản phẩm mới nhất Nam
+exports.getNewProductsMale = async (req, res) => {
+  try {
+    const products = await Product.findAll({
+      where: { gioi_tinh: "Nam" },
+      order: [["createdAt", "DESC"]],
+      limit: 10,
+    });
+
+    if (products.length === 0) {
+      return res.status(404).json({ message: "Không có sản phẩm nào" });
+    }
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// show sản phẩm mới nhất nu
+exports.getNewProductsFeMale = async (req, res) => {
+  try {
+    const products = await Product.findAll({
+      where: { gioi_tinh: "Nữ" },
+      order: [["createdAt", "DESC"]],
+      limit: 10,
+    });
+
+    if (products.length === 0) {
+      return res.status(404).json({ message: "Không có sản phẩm nào" });
+    }
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// show sản phẩm mới nhất Đôi
+exports.getNewProductsCouple = async (req, res) => {
+  try {
+    const products = await Product.findAll({
+      where: { gioi_tinh: "Đồng Hồ Đôi" },
+      order: [["createdAt", "DESC"]],
+      limit: 10,
+    });
+
+    if (products.length === 0) {
+      return res.status(404).json({ message: "Không có sản phẩm nào" });
+    }
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+
+
+// Lấy danh mục theo giới tính "Nam"
+exports.getMale = async (req, res) => {
+  try {
+    const cates = await Product.findAll({
+      where: { gioi_tinh: "Nam" },
+    });
+    res.json(cates);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// Lấy danh mục theo giới tính "Nam" 10sp
+exports.getMale10sp = async (req, res) => {
+  try {
+    const cates = await Product.findAll({
+      where: { gioi_tinh: "Nam" },
+      limit:10
+    });
+    res.json(cates);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// Lấy danh mục theo giới tính "Nữ"
+exports.getFeMale = async (req, res) => {
+  try {
+    const cates = await Product.findAll({
+      where: { gioi_tinh: "Nữ" },
+    });
+    res.json(cates);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// Lấy danh mục theo giới tính "Nữ"10sp
+exports.getFeMale10sp = async (req, res) => {
+  try {
+    const cates = await Product.findAll({
+      where: { gioi_tinh: "Nữ" },
+      limit:10
+    });
+    res.json(cates);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// Lấy danh mục theo "Đôi"
+exports.getCouple = async (req, res) => {
+  try {
+    const cates = await Product.findAll({
+      where: { gioi_tinh: "Đồng Hồ Đôi" },
+    });
+    res.json(cates);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// Lấy danh mục theo giới tính "đồng hồ đôi"
+exports.getCouple10sp = async (req, res) => {
+  try {
+    const cates = await Product.findAll({
+      where: { gioi_tinh: "Đồng Hồ Đôi" },
+      limit:10
+    });
+    res.json(cates);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// lấy sản phẩm dưới 2 c
+exports.getProductsUnderTwoMillion = async (req, res) => {
+  try {
+    const products = await Product.findAll({
+      where: {
+        gia_san_pham: {
+          [Op.lt]: 2000000, // Sản phẩm có giá nhỏ hơn 2 triệu
+        },
+      },
+    });
+
+    if (products.length === 0) {
+      return res.status(404).json({ message: "Không tìm thấy sản phẩm nào dưới 2 triệu" });
+    }
+
+    res.json(products);
+  } catch (error) {
+    console.error("sản phẩm dưới 2 triệu", error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+//lấy sản phẩm từ 2 đến 5 triệu
+exports.getProductstu2den5trieu= async (req, res) => {
+  try {
+    const products = await Product.findAll({
+      where: {
+        gia_san_pham: {
+          [Op.between]: [2000000, 5000000], 
+        },
+      },
+    });
+
+    if (products.length === 0) {
+      return res.status(404).json({ message: "Không tìm thấy sản phẩm nào từ 2 đến 5 triệu" });
+    }
+
+    res.json(products);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+//lấy sản phẩm từ 5 đến 10 triệu
+exports.getProductstu5den10trieu = async (req, res) => {
+  try {
+    const products = await Product.findAll({
+      where: {
+        gia_san_pham: {
+          [Op.between]: [5000000, 10000000],
+        },
+      },
+    });
+
+    if (products.length === 0) {
+      return res
+        .status(404)
+        .json({ message: "Không tìm thấy sản phẩm nào từ 2 đến 5 triệu" });
+    }
+
+    res.json(products);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+//lấy sản phẩm từ 10 đến 20 triệu
+exports.getProductstu10den20trieu = async (req, res) => {
+  try {
+    const products = await Product.findAll({
+      where: {
+        gia_san_pham: {
+          [Op.between]: [10000000, 20000000],
+        },
+      },
+    });
+
+    if (products.length === 0) {
+      return res
+        .status(404)
+        .json({ message: "Không tìm thấy sản phẩm nào từ 2 đến 5 triệu" });
+    }
+
+    res.json(products);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+//lấy sản phẩm từ 20 đến 30 triệu
+exports.getProductstu20den30trieu = async (req, res) => {
+  try {
+    const products = await Product.findAll({
+      where: {
+        gia_san_pham: {
+          [Op.between]: [20000000, 30000000],
+        },
+      },
+    });
+
+    if (products.length === 0) {
+      return res
+        .status(404)
+        .json({ message: "Không tìm thấy sản phẩm nào từ 2 đến 5 triệu" });
+    }
+
+    res.json(products);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
+//lấy sản phẩm từ 30 đến 50 triệu
+exports.getProductstu30den50trieu = async (req, res) => {
+  try {
+    const products = await Product.findAll({
+      where: {
+        gia_san_pham: {
+          [Op.between]: [30000000, 50000000],
+        },
+      },
+    });
+
+    if (products.length === 0) {
+      return res
+        .status(404)
+        .json({ message: "Không tìm thấy sản phẩm nào từ 2 đến 5 triệu" });
+    }
+
+    res.json(products);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
+//lấy sản phẩm từ 50 đến 100 triệu
+exports.getProductstu50den100trieu = async (req, res) => {
+  try {
+    const products = await Product.findAll({
+      where: {
+        gia_san_pham: {
+          [Op.between]: [50000000, 100000000],
+        },
+      },
+    });
+
+    if (products.length === 0) {
+      return res
+        .status(404)
+        .json({ message: "Không tìm thấy sản phẩm nào từ 2 đến 5 triệu" });
+    }
+
+    res.json(products);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
+//lấy sản phẩm treen 100 triệu
+exports.getProductsOver100trieu = async (req, res) => {
+  try {
+    const products = await Product.findAll({
+      where: {
+        gia_san_pham: {
+          [Op.gt]: [ 100000000],
+        },
+      },
+    });
+
+    if (products.length === 0) {
+      return res
+        .status(404)
+        .json({ message: "Không tìm thấy sản phẩm nào từ 2 đến 5 triệu" });
+    }
+
+    res.json(products);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// Lấy danh mục theo chat liệu dây da
+exports.getChatLieuDayDa = async (req, res) => {
+  try {
+    const cates = await Product.findAll({
+      where: { chat_lieu_day: "Dây da" }, // Ensure the value matches the ENUM casing
+    });
+    res.json(cates);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+// Lấy danh mục theo chat liệu dây dù
+exports.getChatLieuDayDu = async (req, res) => {
+  try {
+    const cates = await Product.findAll({
+      where: { chat_lieu_day: "Dây dù" }, // Ensure the value matches the ENUM casing
+    });
+    res.json(cates);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+
 // Lấy tất cả sản phẩm
 exports.getAllProducts = async (req, res) => {
   try {
