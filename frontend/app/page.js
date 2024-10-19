4f0a106100d9139fddc51aa299a3470e91958af7
huutin000 <huutin108@gmail.com>
2024-10-19 16:02:16 +0700

    sửa lỗi chi tiết

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,10 +1,9 @@
 import DonghoNam from "./components/donghonam/page";
-import Login from "./components/login/page";
 
 export default function Home() {
   return (
     <>
-      <Login />
+      <DonghoNam />
     </>
   );
 }
