44307226971bba0776ae97fcfed765ba1c100edb
beola134 <nguyentantai612004@gmail.com>
2024-10-28 20:01:20 +0700

    chỉnh sửa cmt

--- backend/controllers/commentController.js
+++ backend/controllers/commentController.js
@@ -48,25 +48,6 @@ exports.getAllComment = async (req, res) => {
     }
 }
 
-//lấy tất cả bình luận theo _id người dùng
-exports.getAllCommentUser = async (req, res) => {
-    try {
-        const { id_nguoi_dung } = req.params;
-        //kiểm tra xem người dùng có tồn tại không
-        const user = await Users.findOne({ where: { _id: id_nguoi_dung } });
-        if (!user) {
-            return res.status(400).json({ message: "Không tìm thấy người dùng" });
-        }
-        //lấy tất cả bình luận theo _id người dùng
-        const comments = await CMT.findAll({ where: { id_nguoi_dung } });
-        res.status(200).json({comments});
-    }
-    catch (error) {
-        console.error(error.message);
-        res.status(500).json({ message: "Lỗi server" });
-    }
-}
-
 //sửa bình luận theo _id nguoi_dung và _id sản phẩm
 exports.editComment = async (req, res) => {
     try {
