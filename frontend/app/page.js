c8b30d95132f81aa6a58c7838894c2d9bf981c99
huutin000 <huutin108@gmail.com>
2024-11-11 19:56:43 +0700

    fix lỗi github

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,14 +1,9 @@
-import Daydongho from "./components/daydongho/page";
-import DonghoDoi from "./components/donghodoi/page";
 import Main from "./components/layout/trangchu/page";
 
 export default function Home() {
   return (
     <>
-      {/*<ThanhToan/>*/}
       <Main />
-     {/* <DonghoDoi /> */}
-     {/* <Daydongho /> */}
     </>
   );
 }
