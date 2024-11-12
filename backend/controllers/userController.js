dc1347cc5867f01a864f735a5d2d010da95289a0
beola134 <nguyentantai612004@gmail.com>
2024-11-12 22:11:26 +0700

    fix

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -28,33 +28,12 @@ exports.getUserById = async (req, res) => {
 // API lấy thông tin tất cả người dùng
 exports.getAllUsers = async (req, res) => {
   try {
-    const { ten_dang_nhap, limit = 5, page = 1 } = req.query;
-    let filter = {
-      [Op.and]: [],
-    };
-    if (ten_dang_nhap) {
-      filter[Op.and].push({ ten_dang_nhap: { [Op.substring]: ten_dang_nhap } });
-    }
-    const offset = (page - 1) * limit;
-    const userCount = await Users.count({ where: filter });
-    if (userCount <= 5) {
-      const users = await Users.findAll({ where: filter });
-      return res.json({ users, totalUsers: userCount });
-    }
-    const { rows: users, count: totalUsers } = await Users.findAndCountAll({
-      where: filter,
-      limit: parseInt(limit),
-      offset: parseInt(offset),
-    });
-    const totalPage = Math.ceil(totalUsers / limit);
-    res.json({
-      users,
-      currentPage: parseInt(page),
-      totalPage,
-      totalUsers,
-    });
+    const users = await Users.findAll();
+    res.status(200).json({ users });
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({
+      message: error.message,
+    });
   }
 }
 
